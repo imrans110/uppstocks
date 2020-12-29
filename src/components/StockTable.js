0ecc1d105a63ec1d463cafb478bfe3277a6b6898
Syed Imran Abidi <imrans110@hotmail.com>
2020-12-30 01:50:25 +0530

    Handle empty response from WebSocket

--- src/components/StockTable.js
+++ src/components/StockTable.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Table } from "semantic-ui-react";
+import { Container, Table } from "semantic-ui-react";
 import dayjs from "dayjs";
 
 import StockModal from "./StockModal";
+import { isObjectEmpty } from "../utils/helper";
 
 const StockTable = ({ stockData }) => {
   const isNegative = (stockDetails) => {
@@ -33,26 +34,30 @@ const StockTable = ({ stockData }) => {
       </Table.Header>
 
       <Table.Body>
-        {Object.keys(stockData).map((stock) => (
-          <Table.Row
-            key={stock}
-            negative={isNegative(stockData[stock])}
-            positive={isPositive(stockData[stock])}
-          >
-            <Table.Cell>{stock.toUpperCase()} </Table.Cell>
-            <Table.Cell>
-              {stockData[stock].slice(-1)[0].value.toLocaleString()}
-            </Table.Cell>
-            <Table.Cell>
-              {dayjs(stockData[stock].slice(-1)[0].date).format(
-                "DD MMM h:mm a"
-              )}
-            </Table.Cell>
-            <Table.Cell>
-              <StockModal stockDetails={stockData[stock]} stock={stock} />
-            </Table.Cell>
-          </Table.Row>
-        ))}
+        {isObjectEmpty(stockData) ? (
+          <Container> No Data </Container>
+        ) : (
+          Object.keys(stockData).map((stock) => (
+            <Table.Row
+              key={stock}
+              negative={isNegative(stockData[stock])}
+              positive={isPositive(stockData[stock])}
+            >
+              <Table.Cell>{stock.toUpperCase()} </Table.Cell>
+              <Table.Cell>
+                {stockData[stock].slice(-1)[0].value.toLocaleString()}
+              </Table.Cell>
+              <Table.Cell>
+                {dayjs(stockData[stock].slice(-1)[0].date).format(
+                  "DD MMM h:mm a"
+                )}
+              </Table.Cell>
+              <Table.Cell>
+                <StockModal stockDetails={stockData[stock]} stock={stock} />
+              </Table.Cell>
+            </Table.Row>
+          ))
+        )}
       </Table.Body>
     </Table>
   );
